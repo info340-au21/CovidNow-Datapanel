bd275a12d95f73028895ebb0b7c3e7818f738bdd
ratikkoka <koka.ratik@yahoo.com>
2021-11-22 14:26:17 -0800

    Added dependencies and NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,43 @@
 import React from 'react';
+import { NavBar } from './Nav';
+import '../style.css';
+import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
 
 export default function App() {
-  return (<h1>Covid data panel</h1>)
+  return (
+    <div>
+      <header>
+        <NavBar />
+      </header>
+
+      <main>
+        <Switch>
+          <Route exact path="/">
+            
+          </Route>
+
+          <Route path="/about">
+            
+          </Route>
+
+          <Route path="/resources">
+            
+          </Route>
+
+          <Route path="/adopt/:params">
+            
+          </Route>
+
+          <Redirect to="/"/>
+        </Switch>
+      </main>
+
+      <footer>
+            <p className="copyright-text">
+                Copyright &copy; 2021 All Rights Reserved by
+                <NavLink to="/about"> Data Panel Project Team Members</NavLink>.
+            </p>
+        </footer>
+    </div>
+  );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export function NavBar() {
+    return (
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark">
+            <NavLink className="navbar-brand" exact to="/">
+                <img
+                    src="./img/logo.svg"
+                    className="d-inline-block align-top"
+                    alt="Database Logo"
+                />
+            </NavLink>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item">
+                        <NavLink className="nav-link" activeClass="active" exact to="/">Overview</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            className="nav-link"
+                            data-toggle="collapse"
+                            data-target=".multi-collapse"
+                            href="#collapseDashboard"
+                            role="button"
+                            aria-expanded="false"
+                            aria-controls=".multi-collapse"
+                            activeClass="active"
+                            to="/dashboard"
+                            >Dashboard</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link" activeClass="active" to="/about">About Us</NavLink>
+                    </li>
+                </ul>
+                <div className="nav navbar-nav navbar-right">
+                    <NavLink className="nav-item nav-link" activeClass="active" to="/login">
+                        <span className="material-icons">login</span> Login
+                    </NavLink>
+                </div>
+            </div>
+        </nav>
+    );
+  }
