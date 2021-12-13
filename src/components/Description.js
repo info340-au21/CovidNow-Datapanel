cdd33988fe578df7e03be6cac8f53774b71516b7
Jinwoo Kim <jinwookim9911@gmail.com>
2021-12-13 13:20:15 -0800

    alt changes for img tags in description page

--- src/components/Description.js
+++ src/components/Description.js
@@ -30,7 +30,7 @@ export function RealTime() {
         <div className="mt-5 pb-5 description-img">
             <h2>Real Time Data!</h2>
             <p>Our COVID-19 data is provided by an API from Covid Act Now.</p>
-            <a href="https://covidactnow.org/" target="_blank"><img src="../img/CovidActNowLogo.png" alt="d" /></a>
+            <a href="https://covidactnow.org/" target="_blank"><img src="../img/CovidActNowLogo.png" alt="covid act now logo" /></a>
             <p>-</p>
             <a href="https://covidactnow.org/data-api" target="_blank">covidactnow.org/data-api</a>
             <p>-</p>
@@ -47,9 +47,9 @@ export function OverviewPageDemo() {
         <div className="mt-5 description-img">
             <h2>What's included in our Overview page?</h2>
             <p>Left includes a banner with an up-to-date total COVID-19 cases and deaths for USA.</p>
-            <img src="../img/OverviewLeftBanner.png" alt="d" />
+            <img src="../img/OverviewLeftBanner.png" alt="overview stastics" />
             <p>Right includes a state map with a functioning interactive map that shows various COVID-19 data for each state.</p>
-            <img src="../img/OverviewRightBanner.png" alt="d" />
+            <img src="../img/OverviewRightBanner.png" alt="overview map" />
         </div>
     )
 }
@@ -59,15 +59,15 @@ export function InteractiveMap() {
         <div className="mt-5 description-img">
             <h2>Interactive Map!</h2>
             <p>Our interactive map is provided by Mapbox GL JS from Mapbox.</p>
-            <a href="https://www.mapbox.com/" target="_blank"><img src="../img/MapboxLogo.svg" alt="d" /></a>
+            <a href="https://www.mapbox.com/" target="_blank"><img src="../img/MapboxLogo.svg" alt="mapbox logo" /></a>
             <p>-</p>
             <a href="https://docs.mapbox.com/mapbox-gl-js/guides/" target="_blank">docs.mapbox.com/mapbox-gl-js</a>
             <p>-</p>
             <p>A library for building customizable, user-interactive map for web applications.</p>
             <p>Thanks to Mapbox, we are able to easily visualize our COVID-19 for each states.</p>
             <div className="map-gif">
-                <img src="../img/movement.gif" alt="s" />
-                <img src="../img/statehover.gif" alt="s" />
+                <img src="../img/movement.gif" alt="dragging map" />
+                <img src="../img/statehover.gif" alt="state statistics" />
             </div>
         </div>
     )
@@ -79,30 +79,13 @@ export function DescriptionDashboard() {
             <h2>Dashboard.</h2>
             <p>When user clicks on a state from the overview page, the web brings the user to the dashboard page,</p>
             <div className="dashboard-top-img">
-                <img src="../img/DashboardTop.png" alt="se" />
+                <img src="../img/DashboardTop.png" alt="dashboard statstics" />
             </div>
             <p>The dashboard page includes a comprehensive COVID-19 data & interactive COVID-19 data chart for the chosen state.</p>
             <div className="map-gif">
-                <img src="../img/FirstTrendChart.gif" alt="s" />
-                <img src="../img/SecondTrendChart.gif" alt="s" />
+                <img src="../img/FirstTrendChart.gif" alt="dashboard chart one" />
+                <img src="../img/SecondTrendChart.gif" alt="dashboard chart two" />
             </div>
-        </div>
-    )
-}
-
-export function DescriptionWidgets() {
-    return (
-        <div>
-            <h2>test</h2>
-        </div>
-    )
-}
-
-export function DescriptionLogin() {
-    return (
-        <div>
-            <h2>test</h2>
-            <p>Using firebase, the Login function allows the users to save a preffered account, making it convenience for returning users.</p>
         </div>
     )
 }
@@ -110,7 +93,7 @@ export function DescriptionLogin() {
 export function DirectToAbout() {
     return (
         <div>
-            <p>To learn more about our project, head over to the <Link to="/about">About Us</Link> section.</p>
+            <p>(To learn more about our project, head over to the <Link to="/about">About Us</Link> section)</p>
         </div>
     )
 }
