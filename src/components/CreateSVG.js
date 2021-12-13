71b9cfd330f879f8ec38651831906dcb6ce66b09
Danell Forbes <danellsaucy@gmail.com>
2021-12-12 22:53:18 -0800

    fixes

--- src/components/CreateSVG.js
+++ src/components/CreateSVG.js
@@ -2,7 +2,7 @@ import React from "react";
 import * as d3 from 'd3';
 
 
-export default function CreateSVG({props}) {
+export default function CreateSVG(props) {
 
     let data = props.data;
     console.log(data);
